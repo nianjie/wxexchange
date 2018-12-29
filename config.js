a2a1f1525d506791ebb89cc6dc933ebbd95daab0
developer1 <nianjie@hotmail.co.jp>
2018-12-29 16:10:08 +0900

    declare config vars.

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const CONF = {
     rootPathname: '',
 
     // 微信小程序 App ID
-    appId: process.env.wxAppId,
+    appId: process.env.WxAppId,
 
     // 微信小程序 App Secret
-    appSecret: process.env.wxAppSecret,
+    appSecret: process.env.WxAppSecret,
 
     // 是否使用腾讯云代理登录小程序
     useQcloudLogin: false,
